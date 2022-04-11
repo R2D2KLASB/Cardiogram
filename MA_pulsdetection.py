7172a42e808c9241db4d949aca6fec1ab882739f
AhmadSalaymeh1998 <57137669+AhmadSalaymeh1998@users.noreply.github.com>
2022-04-11 15:33:33 +0100

    Add files via upload

--- MA_pulsdetection.py
+++ MA_pulsdetection.py
@@ -1,38 +1,80 @@
-import random
+#import random
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage import feature
 from scipy.ndimage.filters import maximum_filter
+import serial
+import time
 
+ser = serial.Serial('/dev/ttyUSB0', 230400 , timeout=0.1)
+time.sleep(3)
 
 lst=[]
-lst=[1,2,3,4,6,12,6,3,2,1,2,1,5,5,4,3,5,5,6,6,7,20,8,5,6,5,7,5,4,4,5,3,2,1,2,3,1,2,1,2,1,0,2,3,4,5,3,5,7,6,7,8,7,9,11,12,15,20,30,20,15,12,16,12,10,12,10,4,10,18,1,0,0,0,0,0,0,0,0]
+t=time.time()
+
+loop= True
+while loop:
+#for i in range(0,1000):
+    line = ser.readline()   # read a byte
+    if line:
+        string = line.decode(errors='ignore')  # convert the byte string to a unicode string
+        num = int(string)
+        if len(lst) > 0 and num<800:
+            if num-lst[-1]>100 or num-lst[-1]>-100:# convert the unicode string to an int
+                lst.append(num)
+        else:
+            lst.append(num)
+            #print(num)
+    if time.time() - t > 3:
+        loop=False    
+    #time.sleep(0.01)
+
+print(len(lst))
+#lst=[1,2,3,4,6,12,6,3,2,1,2,1,5,5,4,3,5,5,6,6,7,20,8,5,6,5,7,5,4,4,5,3,2,1,2,3,1,2,1,2,1,0,2,3,4,5,3,5,7,6,7,8,7,9,11,12,15,20,30,20,15,12,16,12,10,12,10,4,10,18,1,0,0,0,0,0,0,0,0]
 #
 # for i in range(0,100):
 #     n = random.random() * 10
 #     lst.append(n)
+def movingAvg(LST):
+    MaLst = []
+    MaLst.append(LST[0])
+    MaLst.append(LST[1])
+    MaLst.append(LST[2])
+    MaLst.append(LST[3])
+    MaLst.append(LST[4])
+    MaLst.append(LST[5])
+    MaLst.append(LST[6])
+    MaLst.append(LST[7])
+    MaLst.append(LST[8])
 
-MaLst = []
-MaLst.append(lst[0])
-MaLst.append(lst[1])
-MaLst.append(lst[2])
-MaLst.append(lst[3])
+    for i in range(9, len(LST), 1):
+        MA = (LST[i] + LST[i-1] + LST[i-2] + LST[i-3] + LST[i-4]+LST[i-5] + LST[i-6] + LST[i-7] + LST[i-8] + LST[i-9]) / 10
+        MaLst.append(int(MA))
+    return MaLst
 
-for i in range(4, len(lst), 1):
-    MA = (lst[i] + lst[i-1] + lst[i-2] + lst[i-3] + lst[i-4]) / 5
-    MaLst.append(MA)
 
+minimum = min(lst)
+for i in range(len(lst)):
+    lst[i] -= minimum
+
+maximum = max(lst)
+for i in range(len(lst)):
+    lst[i] *= (25000/maximum)
+MaLsT = movingAvg(lst)
+
+    
+print(MaLsT[50])
+MaLst = movingAvg(MaLsT)
 measurements=[]
 for i in range(0,len(lst),1):
-    measurements.append(i)
+    measurements.append(i * 15)
 
 plt.plot(measurements,MaLst, label = "Moving average(5)")#, linestyle="-.")
 
 # put your data here
 data = np.array(lst)
 
-filter_win_size = 12
-peak_intensity_threshold = 5
+filter_win_size = 15
+peak_intensity_threshold = 6
 
 max_data = maximum_filter(data, filter_win_size)
 min_data = -maximum_filter(-data, filter_win_size)
@@ -48,10 +90,26 @@ peak_mask = np.correlate(peak_mask, [-1, 1], mode='same') == 2
 
 max_places = np.where(peak_mask)[0]
 
-# fig, ax = plt.subplots()
+
+gcodelist =[measurements, MaLst]
+def listToGcode(list):
+    f = open("heartbeat.gcode", "w")
+
+    for i in range(len(list[0])):
+        x = str(list[0][i])
+        y = str(list[1][i])
+    
+        f.write("G1 X" + x + " Y"+ y + " ;\n")
+    f.close()
+listToGcode(gcodelist)
+
+
+
+fig, ax = plt.subplots()
 r = range(data.shape[0])
-plt.plot(r, data, 'k')
-plt.plot(max_places, data[max_places], 'xr')
-plt.grid()
-plt.axis((0, 100, 0, 12))
+ax.plot(r, data, 'k')
+ax.plot(max_places, data[max_places], 'xr')
+ax.grid()
+ax.axis((0, len(lst), min(lst), max(lst)))
 plt.show()
+
