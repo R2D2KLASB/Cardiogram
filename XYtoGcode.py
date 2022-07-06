a4ffe4bb0e1fcf0b2acf85ba0202ed5a7da6e2a6
martijnkootje <66223803+martijnkootje@users.noreply.github.com>
2022-07-06 13:55:41 +0200

    Documentation

--- XYtoGcode.py
+++ XYtoGcode.py
@@ -1,6 +1,8 @@
 def listToGcode(list):
+    # Create a new file
     f = open("heartbeat.gcode", "w")
 
+    # From the given lists with results from the hearbeat sensor, Create useable gcode for the robot
     for i in range(len(list[0])):
         x = str(list[0][i])
         y = str(list[1][i])
